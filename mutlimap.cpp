77556e03e5df06d9562f9ac6f38eaaaf78dfd078
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-15 17:39:47 +0000

    add

--- mutlimap.cpp
+++ mutlimap.cpp
@@ -1,3 +1,24 @@
 /*multi map stores duplicate key but in sorted order*/
 
 /*sort(a,a+1,comp) a->first element a+1->second element comp self writen comparator */
+
+void ex(){
+    bool cmp(pair<int,int>p1,pair<int,int>p2){
+        if(p1.second<p2.second){
+            return true;
+
+        }
+        else if(p1.second>p2.second) 
+        {
+            return false;
+        }
+        else{
+          return true;
+
+        }
+    }
+}
+sort(p1,p2,comp);
+
+//built pop count
+int cnt = builtin_popcountll();
