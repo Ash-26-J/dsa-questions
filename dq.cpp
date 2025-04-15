b90c420e8ea0dccf41132401eeaf3e90e7d664eb
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-15 15:46:13 +0000

    add

---
+++ dq.cpp
@@ -0,0 +1,13 @@
+//deque double ended queue
+#include<bits/stdc++.h>
+void deque(){
+deque<int>dq;
+dq.push_back()
+dq.pop_back()
+dq.push_front()
+dq.pop_front()
+dq.emplace_front()
+dq.emplace_back()
+
+
+}
---
+++ list.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+void listner(){
+list<int>ls;
+l.push_back()
+l.pop_back()
+l.push_front()
+l.pop_front()
+l.emplace_front()
+l.emplace_back()
+}
