16a3da852266809e3dfe074a00aea29cebf618e2
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-15 15:07:07 +0000

    add

---
+++ stl.cpp
@@ -0,0 +1,18 @@
+/*#include<iostream>
+#include<math.h>
+
+using namespace std;
+int main(){
+    cout<<"good george our king";
+    return 0;
+}*/
+
+#include<bits/stdc++.h>
+using namespace std;
+/*
+int main(){
+    cout<<"god bless me the king";
+     return 0;
+
+     
+    
