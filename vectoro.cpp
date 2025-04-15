ebf05c1c0c2ca0e6c21be87165a7adbfb4099611
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-15 15:34:03 +0000

    add

---
+++ vectoro.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+ //vector<int>v1; syntax v -> can be anything
+ /*v.push_back(1) takes 1
+   v.emplace(2)-> {1}->{1,2} dynamically increases and push back
+   v.push_back{1,2}
+   v.emplace_back{1,2}
+
+   vector<int> v{5,100}->5 instances of 100
+   vector<int> v(5) can be dynamically increased
+ 
+ vector<int> :: iterator it = v.begin();
+  
+ vector<int> :: iterator it = v.end();
+  
+ vector<int> :: iterator it = v.rend();
+  
+ vector<int> :: iterator it = v.rbegin();
+
+ for(
+ vector<int> :: iterator it = v.begin();,it != vector<int> :: iterator it = v.end();it++){
+ cout<<*(it)<<"";
+ }
+ for(auto it = v.begin();it != v.end();it++) auto = vector<int> :: iterator
+
+ v.erase()
+ v.insert()
+ ex - v.insert(v.begin() +1,2,10).
+
+ v.pop_back()//pops last element
+ v.clear()// erase the entire vector
+ cout<<v.empty();
+v.size // size of vector
+v1.swap(v2)// swap function
+
+
+ */
