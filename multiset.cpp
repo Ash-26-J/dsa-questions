059f53d709cfb8071b3d50559f5dadbec306ca03
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-15 16:10:00 +0000

    add

---
+++ multiset.cpp
@@ -0,0 +1,8 @@
+void mset(){
+    multiset<int>ms;
+    // if errase 1 then errases all 1 if insert 1 it will go on inserting any number of 1s
+    ms.insert();
+    ms.erase();
+    ms.find();
+    
+}
--- set.cpp
+++ set.cpp
@@ -1,1 +1,15 @@
 // set = sort + unique
+void con(){
+    set<int>st;
+    st.intsert();
+    st.erase();
+    st.count();
+    st.find();
+    st.begin()
+    st.end()
+    st.swap()
+    st.rbegin()
+    st.rend()
+    
+
+}
